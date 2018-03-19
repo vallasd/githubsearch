624d26aa832f3798917c22c7a33b6b51a1082943
David Vallas <david.vallas@michaelkors.com>
2018-03-19 17:27:57 -0400

    created storyboard, cell layout, and cell classes

---
+++ Cells/RepositoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  RepositoryCell.swift
+//  Githubsearch
+//
+//  Created by David Vallas on 3/19/18.
+//  Copyright © 2018 David Vallas. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var stars: UILabel!
+    @IBOutlet weak var forks: UILabel!
+    @IBOutlet weak var updated: UILabel!
+}
+
